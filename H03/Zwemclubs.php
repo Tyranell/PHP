368adf7317e27d11d7c3396c339ead426d2a09aa
Damian Jordan <damianjordan83@gmail.com>
2022-06-01 10:22:48 +0200

    Hoofdstuk 3 af

--- H03/Zwemclubs.php
+++ H03/Zwemclubs.php
@@ -1,14 +1,18 @@
 <?php
     $zwemclubs = array(
-        array('De spartelkuikens', 25),
-        array('De waterbuffels', 32),
-        array('Plonsmderin', 11),
-        array('Bommetje', 23)
+        array('naam' => 'De spartelkuikens', 'aantal' => 25),
+        array('naam' => 'De waterbuffels', 'aantal' => 32),
+        array('naam' => 'Plonsmderin', 'aantal' => 11),
+        array('naam' => 'Bommetje', 'aantal' => 23)
     );
 
-    foreach ($zwemclubs as $name => $zwemclub) {
-        foreach($zwemclub as $num => $value) {
-            $num;
+    foreach ($zwemclubs as $row) {
+        echo $row['naam']. ": " . $row['aantal'] . " ";
+        if (round($row['aantal'] / 5, 0) > 1) {
+            for ($i = 0; $i < floor($row['aantal'] / 5); $i++) {
+                echo '<img src="images/swimIcon.png" alt="SwimIcon" style="max-height: 50px;max-width: 50px">';
+            }
+            echo "<br/>";
         }
     }
 ?>
