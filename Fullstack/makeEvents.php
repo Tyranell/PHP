928f4097cf94f2fdaf301dd8c822b7f5bbbf0718
Damian Jordan <damianjordan83@gmail.com>
2023-09-04 12:47:47 +0200

    1st push na de vakantie wooooooooooo

--- Fullstack/makeEvents.php
+++ Fullstack/makeEvents.php
@@ -1,8 +1,8 @@
 <?php
     $date = $_GET['datum'];
-    $openingTime = $_GET['aavangstijd'];
+    $openingTime = $_GET['aanvangstijd'];
     $nameEvent = $_GET['naamEvent'];
     $price = $_GET['prijs'];
 
-    
+    print_r($_GET);
 ?>
