7ec601a772593443c2d38139b29d179a4f85a40d
Damian Jordan <damianjordan83@gmail.com>
2023-09-14 14:16:17 +0200

    Quack 2: the Quackening

---
+++ Fullstack/connectBandEvent.php
@@ -0,0 +1,69 @@
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=fullstack", $username, $password);
+        // set the PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch(PDOException $e) {
+        echo $sql . "<br>" . $e->getMessage();
+    }
+?>
+
+<head>
+    <style>
+        .flexbox {
+            display: flex;
+            flex-wrap: nowrap;
+        }
+    </style>
+</head>
+<body>
+    <form method="post" class="flexbox">
+        <div> 
+            <select name="bandname" id="bands">
+                <?php
+                // Assuming you've already established a valid database connection
+                $sql1 = "SELECT * FROM band";
+                $result1 = $conn->query($sql1); 
+                if ($result1) { 
+                    while ($row = $result1->fetch(PDO::FETCH_ASSOC)) { 
+                        echo "<option value='" . $row["idband"] . "'>" . $row["naam"] . "</option>"; 
+                        } 
+                    }
+                ?>
+            </select>
+        </div>
+        <div>
+            <?php 
+                $sql2 = "SELECT * FROM event";
+                $result2 = $conn->query($sql2); 
+                if ($result2) { 
+                    while ($row = $result2->fetch(PDO::FETCH_ASSOC)) { 
+                        echo "<input type='checkbox' name='events[]' value='" . $row["idevent"] . "'> " . $row["naam"] . " " . $row["aanvangstijd"] . "<br>";
+                        // echo "<option value='" . $row["idevent"] . "'>" . $row["naam"] . "</option>"; 
+                    } 
+                } 
+            ?>
+        </div>
+        <div>
+            <input type="submit" value="Verstuur">
+        </div>
+    </form>
+</body>
+
+<?php
+    if($_SERVER["REQUEST_METHOD"] == "POST") {
+        $bandname = $_POST['bandname'];
+        $events = $_POST['events'];
+        $evenLeng = count($events);
+        
+        foreach ($events as $event){
+            $sql = "INSERT INTO band_has_event(band_idband, event_idevent) VALUES($bandname, $event)";
+            $conn->exec($sql);
+            echo "New record created successfully.";
+        }
+    } 
+?>
--- Fullstack/enterBand.php
+++ Fullstack/enterBand.php
@@ -2,16 +2,14 @@
     $servername = "localhost";
     $username = "root";
     $password = "";
-    $date = $_GET['datum'];
-    $openingTime = $_GET['aanvangstijd'];
-    $nameEvent = $_GET['naamEvent'];
-    $price = $_GET['prijs'];
+    $name = $_GET['naam'];
+    $genre = $_GET['genre'];
 
     try {
         $conn = new PDO("mysql:host=$servername;dbname=fullstack", $username, $password);
         // set the PDO error mode to exception
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $sql = "INSERT INTO event(datum, naam, entreeprijs, aanvangstijd) VALUES('$date', '$nameEvent', $price, '$openingTime')";
+        $sql = "INSERT INTO band(naam, genre) VALUES('$name', '$genre')";
         $conn->exec($sql);
         echo "New record created successfully.";
     } catch(PDOException $e) {
