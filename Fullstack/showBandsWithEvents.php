9bfab2e05168072fdd92b79eaac8cd611e847b77
Damian Jordan <damianjordan83@gmail.com>
2023-10-02 21:21:34 +0200

    Fullstack done!
    Nu kijken of Piet het mee eens is

--- Fullstack/showBandsWithEvents.php
+++ Fullstack/showBandsWithEvents.php
@@ -57,19 +57,32 @@
                 <th>Entreeprijs</th>
             </tr>
                 <?php 
+                    $previousEvent = null;
                     $sql = "SELECT * FROM band JOIN band_has_event ON band.idband = band_has_event.band_idband JOIN event ON event.idevent = band_has_event.event_idevent";
                     $result = $conn->query($sql);
                     
                     if ($result) {
                         while ($row = $result->fetch(PDO::FETCH_ASSOC)) { 
-                            echo "<tr>";
-                            echo "<td>" . $row["bandNaam"] . "</td>";
-                            echo "<td>" . $row["genre"] . "</td>";
-                            echo "<td>" . $row["eventNaam"] . "</td>"; 
-                            echo "<td>" . $row["datum"] . "</td>"; 
-                            echo "<td>" . $row["aanvangstijd"] . "</td>"; 
-                            echo "<td>" . $row["entreeprijs"] . "</td>";
-                            echo "</tr>";
+                            if ($row["idevent"] != $previousEvent) {
+                                echo "<tr>";
+                                echo "<td>" . $row["eventNaam"] . "</td>";
+                                echo "<td>" . $row["datum"] . "</td>"; 
+                                echo "<td>" . $row["aanvangstijd"] . "</td>"; 
+                                echo "<td>" . $row["entreeprijs"] . "</td>";
+                                echo "<td>" . $row["bandNaam"] . "</td>";
+                                echo "<td>" . $row["genre"] . "</td>";
+                                echo "</tr>";
+                            } else {
+                                echo "<tr>";
+                                echo "<td></td>";  
+                                echo "<td></td>"; 
+                                echo "<td></td>"; 
+                                echo "<td></td>";
+                                echo "<td>" . $row["bandNaam"] . "</td>";
+                                echo "<td>" . $row["genre"] . "</td>";
+                                echo "</tr>";
+                            }
+                            $previousEvent = $row["idevent"];
                         }
                     }
                 ?>
